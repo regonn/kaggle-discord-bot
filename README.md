a7971d6ba2dafd8572f1de7cd81415cd44c9ad59
regonn <programmer@haizine.art>
2019-09-02 19:30:37 +0900

    i18n

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+#### English follows Japanese
+
 Heroku とかにあげて `python main` を実行するとDiscordのチャンネルにKaggleのメダル対象のコンペ情報を流してくれます。
+
+![sample](./sample.png)
 
 <a href="https://heroku.com/deploy?template=https://github.com/regonn/kaggle-discord-bot&env[PRODUCTION]=true">
   <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
@@ -6,13 +10,38 @@ Heroku とかにあげて `python main` を実行するとDiscordのチャンネ
 
 ## 利用している環境変数
 
+次の環境変数を設定してください
+`dotenv` を利用する場合には `.env.sample` を参考に `.env` ファイルを作成してください。
+
 ```
 DISCORD_TOKEN='DISCORDのBOT TOKEN'
 KAGGLE_USERNAME='KaggleのUser名(※)'
 KAGGLE_KEY='KaggleAPI用のKey(※)'
-CHANNEL_ID='DiscordのBOTがつぶやくチャンネル'
+DISCORD_CHANNEL_ID='DiscordのBOTがつぶやくチャンネル'
 PRODUCTION='何も設定されていないと dotenv が呼ばれる開発環境用'
 ```
 
 (※) KAGGLE系の環境変数は `KaggleApi` が呼ばれたタイミングで必要なので dotenv が利用できないです。
 pytho実行環境の環境変数に追加しておくか kaggle.json を設置してください。
+
+## Environment variables
+
+You need these values.
+If you use `dotenv`, please create `.env` from `.env.sample`.
+
+```
+# Now supports English(en) or Japanese(ja).
+LOCALE='en'
+
+# Discord
+DISCORD_TOKEN=
+DISCORD_CHANNEL_ID=
+
+# These variables are needed for setting local environment variables when import kaggle-api.
+# You can also place kaggle.json file in your root directory.
+KAGGLE_USERNAME=
+KAGGLE_KEY=
+
+# If this key is set, load dotenv values.
+# PRODUCTION=True
+```
--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 from datetime import datetime as dt
 import os
 import sys
+import i18n
+from pytz import timezone
 
 if os.environ.get("PRODUCTION") is None:
     load_dotenv(verbose=True)
+
+i18n.set('locale', os.environ.get('LOCALE'))
+i18n.load_path.append('./locale')
 
 client = discord.Client()
 
@@ -19,14 +24,17 @@ api.authenticate()
 @client.event
 async def on_ready():
     channel = client.get_channel(int(os.environ.get("DISCORD_CHANNEL_ID")))
-    await channel.send('おはようございます。UTC 0時です。今日もKaggleやっていきましょ!!')
+    now = dt.now()
+    now = now.astimezone(timezone('UTC'))
+    await channel.send(i18n.t('kaggle.hi', hour=now.hour))
     competitions_list = api.competitions_list()
     for competition in competitions_list:
         if getattr(competition, 'awardsPoints') and not getattr(competition, 'submissionsDisabled'):
             deadline = getattr(competition, 'deadline')
-            diff = deadline - dt.now()
+            deadline = deadline.astimezone(timezone('UTC'))
+            diff = deadline - now
             if diff.days > 0:
-                await channel.send('(あと{}日): {}'.format(diff.days, getattr(competition, 'title')))
+                await channel.send('{}: {}'.format(i18n.t('kaggle.to_go', days=diff.days), getattr(competition, 'title')))
     print('We have logged in as {0.user}'.format(client))
     sys.exit(0)
 
